0c52080b583a8f632b1b95ca0f5c3b9d27d3d48f
ThoonNadySan <6731503090@lamduan.mfu.ac.th>
2025-04-01 09:20:06 +0000

    Done

--- Group_11_Project/src/models/Expense.java
+++ Group_11_Project/src/models/Expense.java
@@ -6,6 +6,13 @@ package models;
  * javac -d out -cp src src/models/Expense.java
  */
 
+ /**
+  * The reasons for using inheritance
+  *Instead of writing the same code multiple times, a child class inherits methods and properties from a parent class.
+  *A subclass can modify the behavior of a method inherited from its superclass.
+  *New features can be added without modifying existing code.
+  */
+
 public class Expense extends Transaction // **Inheritance**: Extends Transaction class
  {
     public Expense(String description, double amount, String category) {
@@ -18,3 +25,5 @@ public class Expense extends Transaction // **Inheritance**: Extends Transaction
         return "Expense";
     }
 }
+    
+
